b421b999a193a026d63e6a61c136ff0f4aaaac9a
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2024-12-11 10:38:00 -0300

    Class Math

---
+++ CONCEPTOS BASICOS/Math/untitled/src/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Raiz cuadrada de 9
+        double raiz = Math.sqrt(9);
+        System.out.println(raiz);
+
+        //Redondeo de numero
+        float num1=5.85F;
+        int resultado = Math.round(num1);
+        System.out.println(resultado);
+
+        //Potencia
+        double base = 5;
+        double exponente = 3;
+
+        double resultado2 = Math.pow(base,exponente);
+        System.out.println(resultado2);
+    }
+}

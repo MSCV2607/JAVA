21d1feceef1c9fcb6b5401d3b3281727a69d7a2f
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-01-06 18:30:18 -0300

    Problemas con Lombok

--- Spring Boot/4 - PostMapping y RequestBody/PostMapping-RequestBody/src/main/java/com/msv/PostMapping/RequestBody/Cliente.java
+++ Spring Boot/4 - PostMapping y RequestBody/PostMapping-RequestBody/src/main/java/com/msv/PostMapping/RequestBody/Cliente.java
@@ -4,9 +4,33 @@ import lombok.Getter;
 import lombok.Setter;
 
 //Gracias a Lombok tengo todos los getters y setters creados
-@Getter @Setter
+//@Getter @Setter
 public class Cliente {
     private String nombre;
     private String apellido;
     private long id;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
 }

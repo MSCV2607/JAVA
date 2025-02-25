01b78187fb690a6bdb1e4bf2dd391d229a0834ce
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-02-25 11:24:09 -0300

    Usando DTO

--- Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserRestController.java
+++ Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserRestController.java
@@ -1,5 +1,6 @@
 package com.MSV.CreacionProy.controllers;
 
+import com.MSV.CreacionProy.DTO.UserDto;
 import com.MSV.CreacionProy.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,19 +15,33 @@ import java.util.Map;
 //Ruta principal
 @RequestMapping("/api")
 public class UserRestController {
-    //ruta secundaria
-    @GetMapping("/details2")
-    public Map<String, Object> details() {
+    //Inutilizado
+//    @GetMapping("/details2")
+//    public Map<String, Object> details() {
+//
+//        User user = new User("Andres","Rolon");
+//
+//
+//        Map<String, Object> body = new HashMap<>();
+//
+//        body.put("title", "Hola mundo con Spring");
+//        body.put("name", user);
+//
+//
+//        return body;
+//    }
+
+    //USANDO DTO
+    @GetMapping("/details1")
+    public UserDto details() {
+
+        UserDto userDto = new UserDto();
 
         User user = new User("Andres","Rolon");
 
+        userDto.setUser(user);
+        userDto.setTitle("Hola mundo con Spring");
 
-        Map<String, Object> body = new HashMap<>();
-
-        body.put("title", "Hola mundo con Spring");
-        body.put("name", user);
-
-
-        return body;
+        return userDto;
     }
 }

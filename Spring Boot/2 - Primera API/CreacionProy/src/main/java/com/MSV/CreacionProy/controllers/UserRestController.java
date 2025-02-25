8e4f2e854cc979cee654eaa9f2e88cc22d24ff1b
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-02-25 10:51:48 -0300

    Objeto usuario

--- Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserRestController.java
+++ Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserRestController.java
@@ -1,22 +1,31 @@
 package com.MSV.CreacionProy.controllers;
 
+import com.MSV.CreacionProy.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+//Ruta principal
+@RequestMapping("/api")
 public class UserRestController {
+    //ruta secundaria
     @GetMapping("/details2")
     public Map<String, Object> details() {
+
+        User user = new User("Andres","Rolon");
+
+
         Map<String, Object> body = new HashMap<>();
 
         body.put("title", "Hola mundo con Spring");
-        body.put("name", "Mario");
-        body.put("lastName", "Vecc");
+        body.put("name", user);
+
 
         return body;
     }

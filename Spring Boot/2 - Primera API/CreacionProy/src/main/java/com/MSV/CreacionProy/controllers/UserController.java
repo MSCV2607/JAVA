0b2406783212a9befeb8e1476da1ebb1dd50d8ae
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-03-04 10:41:20 -0300

    Lista en Thymeleaf

--- Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserController.java
+++ Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserController.java
@@ -1,17 +1,34 @@
 package com.MSV.CreacionProy.controllers;
 
+import com.MSV.CreacionProy.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class UserController {
     @GetMapping("/details")
     public String details(Model model) {
 
-        model.addAttribute("title","Hola Mundo con Spring");
-        model.addAttribute("name","Mauro");
-        model.addAttribute("lastName","Vecc");
+        User user = new User("Mauro","Vecc");
+        user.setEmail("Mauro@Vecc");
+
+        model.addAttribute("title:", "Hola mundo spring");
+        model.addAttribute("user", user);
+
         return "details";
+    }
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+        List<User> users = new ArrayList<>();
+
+        model.addAttribute("users", users);
+        model.addAttribute("title:", "Listado de usuarios");
+        return "list";
     }
 }

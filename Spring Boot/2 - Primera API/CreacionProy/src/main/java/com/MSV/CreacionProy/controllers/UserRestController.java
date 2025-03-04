e31a649fb538db245bea2d27e144ff3b5ea0e8c0
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-03-04 09:47:42 -0300

    API rest list

--- Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserRestController.java
+++ Spring Boot/2 - Primera API/CreacionProy/src/main/java/com/MSV/CreacionProy/controllers/UserRestController.java
@@ -8,32 +8,49 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 //Ruta principal
 @RequestMapping("/api")
 public class UserRestController {
     //Inutilizado
-//    @GetMapping("/details2")
-//    public Map<String, Object> details() {
-//
-//        User user = new User("Andres","Rolon");
-//
-//
-//        Map<String, Object> body = new HashMap<>();
-//
-//        body.put("title", "Hola mundo con Spring");
-//        body.put("name", user);
-//
-//
-//        return body;
-//    }
+    @GetMapping("/details")
+    public Map<String, Object> details() {
+
+        User user = new User("Andres","Rolon");
+
+
+        Map<String, Object> body = new HashMap<>();
+
+        body.put("title", "Hola mundo con Spring");
+        body.put("name", user);
+
+
+        return body;
+    }
+
+    //Devuelve una lista
+    @GetMapping("/list")
+    public List<User> list() {
+        User user1 = new User("Andres","Rolon");
+        User user2 = new User("Pepe","Rol");
+        User user3 = new User("Jhon","Ro");
+
+        //Simplifica lo de abajo
+        List<User> users = Arrays.asList(user1, user2, user3);
+
+//        List <User> users = new ArrayList<>();
+//        users.add(user1);
+//        users.add(user2);
+//        users.add(user3);
+
+        return users;
+    }
 
     //USANDO DTO
-    @GetMapping("/details1")
-    public UserDto details() {
+    @GetMapping("/details-map")
+    public UserDto detailsMap() {
 
         UserDto userDto = new UserDto();
 
